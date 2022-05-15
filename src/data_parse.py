bbbce5ce32e5d8c7e34de128b511c75bdede78d7
Sahil Pattni <sahilpattni97@gmail.com>
2022-05-15 20:57:09 +0400

    Updated path

--- src/data_parse.py
+++ src/data_parse.py
@@ -2,15 +2,16 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('data/ADA.csv')
+df = pd.read_csv('../data/ADA.csv')
 # Sort by date (ascending)
 df.sort_values(by='unix', inplace=True, ascending=True)
-
+# Drop NaN columns
+df.dropna(axis=1, inplace=True)
 # --- Add SMAs --- #
 df['SMA_7'] = df.close.rolling(7).mean()
 df['SMA_25'] = df.close.rolling(25).mean()
 df['SMA_99'] = df.close.rolling(99).mean()
-
+# %%
 # Drop NaN values
 df.dropna(inplace=True)
 # %%
